e5ad0aca148995ed4c0e6bf32aadc50ce4ec9568
Mike C. Fletcher <mcfletch@sturm>
2009-07-18 16:50:44 -0400

    Get the teapots demo working on Linux again

--- PyOpenGL-Demo/redbook/teapots.py
+++ PyOpenGL-Demo/redbook/teapots.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python
-
-# This is statement is required by the build system to query build info
-if __name__ == '__build__':
-	raise Exception
-
 '''
 teapots.py
 Converted to Python by Jason Petrone 6/00
@@ -62,6 +57,7 @@ ERROR: PyOpenGL not installed properly.
         '''
   sys.exit()
 
+teapotList = None
 
 # Initialize depth buffer, projection matrix, light source, and lighting
 # model.  Do not specify a material property here.
@@ -93,7 +89,8 @@ def init():
    teapotList = glGenLists(1)
    glNewList (teapotList, GL_COMPILE)
    glutSolidTeapot(1.0)
-   #glutSolidSphere( 1.0, 8, 8 )
+#   glutWireTeapot(1.0)
+#   glutSolidSphere( 1.0, 8, 8 )
    glEndList ()
 
 # Move object into position.  Use 3rd through 12th 
@@ -111,7 +108,11 @@ def renderTeapot(x, y, ambr, ambg, ambb, difr, difg,
    mat[0] = specr; mat[1] = specg; mat[2] = specb
    glMaterialfv(GL_FRONT, GL_SPECULAR, mat)
    glMaterialf(GL_FRONT, GL_SHININESS, shine * 128.0)
-   glCallList(teapotList)
+   # Linux GLUT seems to optimize away the actual generation of the 
+   # teapot on modern GLUT when using display-lists, so we have to 
+   # use direct calls here...
+#   glCallList(teapotList)
+   glutSolidTeapot(1.0)
    glPopMatrix()
 
 
@@ -120,57 +121,61 @@ def renderTeapot(x, y, ambr, ambg, ambb, difr, difg,
 #  3rd column:  black, cyan, green, red, white, yellow plastic
 #  4th column:  black, cyan, green, red, white, yellow rubber
 def display():
-   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-   renderTeapot(2.0, 17.0, 0.0215, 0.1745, 0.0215,
+    global teapotList
+    if not teapotList:
+        init();
+    glClearColor( 1.0,1.0,1.0, 1.0 )
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    renderTeapot(2.0, 17.0, 0.0215, 0.1745, 0.0215,
       0.07568, 0.61424, 0.07568, 0.633, 0.727811, 0.633, 0.6)
-   renderTeapot(2.0, 14.0, 0.135, 0.2225, 0.1575,
+    renderTeapot(2.0, 14.0, 0.135, 0.2225, 0.1575,
       0.54, 0.89, 0.63, 0.316228, 0.316228, 0.316228, 0.1)
-   renderTeapot(2.0, 11.0, 0.05375, 0.05, 0.06625,
+    renderTeapot(2.0, 11.0, 0.05375, 0.05, 0.06625,
       0.18275, 0.17, 0.22525, 0.332741, 0.328634, 0.346435, 0.3)
-   renderTeapot(2.0, 8.0, 0.25, 0.20725, 0.20725,
+    renderTeapot(2.0, 8.0, 0.25, 0.20725, 0.20725,
       1, 0.829, 0.829, 0.296648, 0.296648, 0.296648, 0.088)
-   renderTeapot(2.0, 5.0, 0.1745, 0.01175, 0.01175,
+    renderTeapot(2.0, 5.0, 0.1745, 0.01175, 0.01175,
       0.61424, 0.04136, 0.04136, 0.727811, 0.626959, 0.626959, 0.6)
-   renderTeapot(2.0, 2.0, 0.1, 0.18725, 0.1745,
+    renderTeapot(2.0, 2.0, 0.1, 0.18725, 0.1745,
       0.396, 0.74151, 0.69102, 0.297254, 0.30829, 0.306678, 0.1)
-   renderTeapot(6.0, 17.0, 0.329412, 0.223529, 0.027451,
+    renderTeapot(6.0, 17.0, 0.329412, 0.223529, 0.027451,
       0.780392, 0.568627, 0.113725, 0.992157, 0.941176, 0.807843,
       0.21794872);
-   renderTeapot(6.0, 14.0, 0.2125, 0.1275, 0.054,
+    renderTeapot(6.0, 14.0, 0.2125, 0.1275, 0.054,
       0.714, 0.4284, 0.18144, 0.393548, 0.271906, 0.166721, 0.2)
-   renderTeapot(6.0, 11.0, 0.25, 0.25, 0.25,
+    renderTeapot(6.0, 11.0, 0.25, 0.25, 0.25,
       0.4, 0.4, 0.4, 0.774597, 0.774597, 0.774597, 0.6)
-   renderTeapot(6.0, 8.0, 0.19125, 0.0735, 0.0225,
+    renderTeapot(6.0, 8.0, 0.19125, 0.0735, 0.0225,
       0.7038, 0.27048, 0.0828, 0.256777, 0.137622, 0.086014, 0.1)
-   renderTeapot(6.0, 5.0, 0.24725, 0.1995, 0.0745,
+    renderTeapot(6.0, 5.0, 0.24725, 0.1995, 0.0745,
       0.75164, 0.60648, 0.22648, 0.628281, 0.555802, 0.366065, 0.4)
-   renderTeapot(6.0, 2.0, 0.19225, 0.19225, 0.19225,
+    renderTeapot(6.0, 2.0, 0.19225, 0.19225, 0.19225,
       0.50754, 0.50754, 0.50754, 0.508273, 0.508273, 0.508273, 0.4)
-   renderTeapot(10.0, 17.0, 0.0, 0.0, 0.0, 0.01, 0.01, 0.01,
+    renderTeapot(10.0, 17.0, 0.0, 0.0, 0.0, 0.01, 0.01, 0.01,
       0.50, 0.50, 0.50, .25)
-   renderTeapot(10.0, 14.0, 0.0, 0.1, 0.06, 0.0, 0.50980392, 0.50980392,
+    renderTeapot(10.0, 14.0, 0.0, 0.1, 0.06, 0.0, 0.50980392, 0.50980392,
       0.50196078, 0.50196078, 0.50196078, .25)
-   renderTeapot(10.0, 11.0, 0.0, 0.0, 0.0,
+    renderTeapot(10.0, 11.0, 0.0, 0.0, 0.0,
       0.1, 0.35, 0.1, 0.45, 0.55, 0.45, .25);
-   renderTeapot(10.0, 8.0, 0.0, 0.0, 0.0, 0.5, 0.0, 0.0,
+    renderTeapot(10.0, 8.0, 0.0, 0.0, 0.0, 0.5, 0.0, 0.0,
       0.7, 0.6, 0.6, .25)
-   renderTeapot(10.0, 5.0, 0.0, 0.0, 0.0, 0.55, 0.55, 0.55,
+    renderTeapot(10.0, 5.0, 0.0, 0.0, 0.0, 0.55, 0.55, 0.55,
       0.70, 0.70, 0.70, .25)
-   renderTeapot(10.0, 2.0, 0.0, 0.0, 0.0, 0.5, 0.5, 0.0,
+    renderTeapot(10.0, 2.0, 0.0, 0.0, 0.0, 0.5, 0.5, 0.0,
       0.60, 0.60, 0.50, .25)
-   renderTeapot(14.0, 17.0, 0.02, 0.02, 0.02, 0.01, 0.01, 0.01,
+    renderTeapot(14.0, 17.0, 0.02, 0.02, 0.02, 0.01, 0.01, 0.01,
       0.4, 0.4, 0.4, .078125)
-   renderTeapot(14.0, 14.0, 0.0, 0.05, 0.05, 0.4, 0.5, 0.5,
+    renderTeapot(14.0, 14.0, 0.0, 0.05, 0.05, 0.4, 0.5, 0.5,
       0.04, 0.7, 0.7, .078125)
-   renderTeapot(14.0, 11.0, 0.0, 0.05, 0.0, 0.4, 0.5, 0.4,
+    renderTeapot(14.0, 11.0, 0.0, 0.05, 0.0, 0.4, 0.5, 0.4,
       0.04, 0.7, 0.04, .078125)
-   renderTeapot(14.0, 8.0, 0.05, 0.0, 0.0, 0.5, 0.4, 0.4,
+    renderTeapot(14.0, 8.0, 0.05, 0.0, 0.0, 0.5, 0.4, 0.4,
       0.7, 0.04, 0.04, .078125)
-   renderTeapot(14.0, 5.0, 0.05, 0.05, 0.05, 0.5, 0.5, 0.5,
+    renderTeapot(14.0, 5.0, 0.05, 0.05, 0.05, 0.5, 0.5, 0.5,
       0.7, 0.7, 0.7, .078125)
-   renderTeapot(14.0, 2.0, 0.05, 0.05, 0.0, 0.5, 0.5, 0.4,
+    renderTeapot(14.0, 2.0, 0.05, 0.05, 0.0, 0.5, 0.5, 0.4,
       0.7, 0.7, 0.04, .078125)
-   glFlush()
+    glutSwapBuffers()
 
 def reshape(w, h):
    glViewport(0, 0, w, h)
@@ -190,11 +195,10 @@ def keyboard(key, x, y):
 # Main Loop 
 if __name__ == "__main__":
 	glutInit(sys.argv);
-	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
 	glutInitWindowSize(500, 600);
 	glutInitWindowPosition(50,50);
 	glutCreateWindow(sys.argv[0]);
-	init();
 	glutReshapeFunc(reshape)
 	glutDisplayFunc(display)
 	glutKeyboardFunc(keyboard)
