a7b4189d5549a8eaa6fa92feb805ad331b3b8704
Mike C. Fletcher <mcfletch@vrplumber.com>
2010-02-22 16:14:19 -0500

    Fix init from sys.argv

--- PyOpenGL-Demo/NeHe/lesson16.py
+++ PyOpenGL-Demo/NeHe/lesson16.py
@@ -14,7 +14,7 @@ ESCAPE = '\033'
 # Number of the glut window.
 window = 0
 
-# Rotations for cube. 
+# Rotations for cube.
 xrot = yrot = zrot = 0.0
 
 fogColor=(0.5, 0.5, 0.5, 1.0) # fog color
@@ -24,14 +24,14 @@ texture = 0
 def LoadTextures():
     #global texture
     image = open("NeHe.bmp")
-    
+
     ix = image.size[0]
     iy = image.size[1]
     image = image.tostring("raw", "RGBX", 0, -1)
-    
-    # Create Texture    
+
+    # Create Texture
     glBindTexture(GL_TEXTURE_2D, glGenTextures(1))   # 2d texture (x and y size)
-    
+
     glPixelStorei(GL_UNPACK_ALIGNMENT,1)
     glTexImage2D(GL_TEXTURE_2D, 0, 3, ix, iy, 0, GL_RGBA, GL_UNSIGNED_BYTE, image)
     glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP)
@@ -42,7 +42,7 @@ def LoadTextures():
     glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
     glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL)
 
-# A general OpenGL initialization function.  Sets all of the initial parameters. 
+# A general OpenGL initialization function.  Sets all of the initial parameters.
 def InitGL(Width, Height):                # We call this right after our OpenGL window is created.
     LoadTextures()
     glEnable(GL_TEXTURE_2D)
@@ -51,7 +51,7 @@ def InitGL(Width, Height):                # We call this right after our OpenGL 
     glDepthFunc(GL_LESS)                # The Type Of Depth Test To Do
     glEnable(GL_DEPTH_TEST)                # Enables Depth Testing
     glShadeModel(GL_SMOOTH)                # Enables Smooth Color Shading
-    
+
     glMatrixMode(GL_PROJECTION)
     glLoadIdentity()                    # Reset The Projection Matrix
                                         # Calculate The Aspect Ratio Of The Window
@@ -61,7 +61,7 @@ def InitGL(Width, Height):                # We call this right after our OpenGL 
 
 # The function called when our window is resized (which shouldn't happen if you enable fullscreen, below)
 def ReSizeGLScene(Width, Height):
-    if Height == 0:                        # Prevent A Divide By Zero If The Window Is Too Small 
+    if Height == 0:                        # Prevent A Divide By Zero If The Window Is Too Small
         Height = 1
 
     glViewport(0, 0, Width, Height)        # Reset The Current Viewport And Perspective Transformation
@@ -70,87 +70,87 @@ def ReSizeGLScene(Width, Height):
     gluPerspective(45.0, float(Width)/float(Height), 0.1, 100.0)
     glMatrixMode(GL_MODELVIEW)
 
-# The main drawing function. 
+# The main drawing function.
 def DrawGLScene():
     global xrot, yrot, zrot, texture
 
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)    # Clear The Screen And The Depth Buffer
     glLoadIdentity()                    # Reset The View
-    
-    glClearColor(0.5,0.5,0.5,1.0)            
-    
+
+    glClearColor(0.5,0.5,0.5,1.0)
+
     glEnable(GL_FOG)                         # Enable fog
-    
+
     glFogi(GL_FOG_MODE, GL_LINEAR)           # Set fog settings
-    
+
     glFogfv(GL_FOG_COLOR, fogColor);         # Set fog color
-    
+
     glFogf(GL_FOG_DENSITY, 0.15);            # Set fog density
-    
+
     glHint(GL_FOG_HINT, GL_DONT_CARE);       # Set Hint
-    
-    glFogf(GL_FOG_START, 1.0);               # Fog start 
-    
+
+    glFogf(GL_FOG_START, 1.0);               # Fog start
+
     glFogf(GL_FOG_END, 5.0);                 # Fog end
 
-    
+
     glTranslatef(0.0,0.0,-5.0)            # Move Into The Screen
 
     glRotatef(xrot,1.0,0.0,0.0)            # Rotate The Cube On It's X Axis
     glRotatef(yrot,0.0,1.0,0.0)            # Rotate The Cube On It's Y Axis
     glRotatef(zrot,0.0,0.0,1.0)            # Rotate The Cube On It's Z Axis
-    
+
     # Note there does not seem to be support for this call.
     #glBindTexture(GL_TEXTURE_2D,texture)    # Rotate The Pyramid On It's Y Axis
 
     glBegin(GL_QUADS)                # Start Drawing The Cube
-    
+
     # Front Face (note that the texture's corners have to match the quad's corners)
     glTexCoord2f(0.0, 0.0); glVertex3f(-1.0, -1.0,  1.0)    # Bottom Left Of The Texture and Quad
     glTexCoord2f(1.0, 0.0); glVertex3f( 1.0, -1.0,  1.0)    # Bottom Right Of The Texture and Quad
     glTexCoord2f(1.0, 1.0); glVertex3f( 1.0,  1.0,  1.0)    # Top Right Of The Texture and Quad
     glTexCoord2f(0.0, 1.0); glVertex3f(-1.0,  1.0,  1.0)    # Top Left Of The Texture and Quad
-    
+
     # Back Face
     glTexCoord2f(1.0, 0.0); glVertex3f(-1.0, -1.0, -1.0)    # Bottom Right Of The Texture and Quad
     glTexCoord2f(1.0, 1.0); glVertex3f(-1.0,  1.0, -1.0)    # Top Right Of The Texture and Quad
     glTexCoord2f(0.0, 1.0); glVertex3f( 1.0,  1.0, -1.0)    # Top Left Of The Texture and Quad
     glTexCoord2f(0.0, 0.0); glVertex3f( 1.0, -1.0, -1.0)    # Bottom Left Of The Texture and Quad
-    
+
     # Top Face
     glTexCoord2f(0.0, 1.0); glVertex3f(-1.0,  1.0, -1.0)    # Top Left Of The Texture and Quad
     glTexCoord2f(0.0, 0.0); glVertex3f(-1.0,  1.0,  1.0)    # Bottom Left Of The Texture and Quad
     glTexCoord2f(1.0, 0.0); glVertex3f( 1.0,  1.0,  1.0)    # Bottom Right Of The Texture and Quad
     glTexCoord2f(1.0, 1.0); glVertex3f( 1.0,  1.0, -1.0)    # Top Right Of The Texture and Quad
-    
-    # Bottom Face       
+
+    # Bottom Face
     glTexCoord2f(1.0, 1.0); glVertex3f(-1.0, -1.0, -1.0)    # Top Right Of The Texture and Quad
     glTexCoord2f(0.0, 1.0); glVertex3f( 1.0, -1.0, -1.0)    # Top Left Of The Texture and Quad
     glTexCoord2f(0.0, 0.0); glVertex3f( 1.0, -1.0,  1.0)    # Bottom Left Of The Texture and Quad
     glTexCoord2f(1.0, 0.0); glVertex3f(-1.0, -1.0,  1.0)    # Bottom Right Of The Texture and Quad
-    
+
     # Right face
     glTexCoord2f(1.0, 0.0); glVertex3f( 1.0, -1.0, -1.0)    # Bottom Right Of The Texture and Quad
     glTexCoord2f(1.0, 1.0); glVertex3f( 1.0,  1.0, -1.0)    # Top Right Of The Texture and Quad
     glTexCoord2f(0.0, 1.0); glVertex3f( 1.0,  1.0,  1.0)    # Top Left Of The Texture and Quad
     glTexCoord2f(0.0, 0.0); glVertex3f( 1.0, -1.0,  1.0)    # Bottom Left Of The Texture and Quad
-    
+
     # Left Face
     glTexCoord2f(0.0, 0.0); glVertex3f(-1.0, -1.0, -1.0)    # Bottom Left Of The Texture and Quad
     glTexCoord2f(1.0, 0.0); glVertex3f(-1.0, -1.0,  1.0)    # Bottom Right Of The Texture and Quad
     glTexCoord2f(1.0, 1.0); glVertex3f(-1.0,  1.0,  1.0)    # Top Right Of The Texture and Quad
     glTexCoord2f(0.0, 1.0); glVertex3f(-1.0,  1.0, -1.0)    # Top Left Of The Texture and Quad
-    
+
     glEnd();                # Done Drawing The Cube
-    
+
     xrot  = xrot + 0.2                # X rotation
     yrot = yrot + 0.2                 # Y rotation
     zrot = zrot + 0.2                 # Z rotation
 
-    #  since this is double buffered, swap the buffers to display what just got drawn. 
+    #  since this is double buffered, swap the buffers to display what just got drawn.
     glutSwapBuffers()
 
-# The function called whenever a key is pressed. Note the use of Python tuples to pass in: (key, x, y)  
+# The function called whenever a key is pressed. Note the use of Python tuples to pass in: (key, x, y)
 def keyPressed(*args):
     # If escape is pressed, kill everything.
     if args[0] == ESCAPE:
@@ -158,21 +158,21 @@ def keyPressed(*args):
 
 def main():
     global window
-    glutInit(sys.argv)
+    glutInit(sys.argv[1:])
 
-    # Select type of Display mode:   
-    #  Double buffer 
+    # Select type of Display mode:
+    #  Double buffer
     #  RGBA color
-    # Alpha components supported 
+    # Alpha components supported
     # Depth buffer
     glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH)
-    
-    # get a 640 x 480 window 
+
+    # get a 640 x 480 window
     glutInitWindowSize(640, 480)
-    
-    # the window starts at the upper left corner of the screen 
+
+    # the window starts at the upper left corner of the screen
     glutInitWindowPosition(0, 0)
-    
+
     # Okay, like the C version we retain the window id to use when closing, but for those of you new
     # to Python (like myself), remember this assignment would make the variable local and not global
     # if it weren't for the global declaration at the start of main.
@@ -180,29 +180,29 @@ def main():
 
        # Register the drawing function with glut, BUT in Python land, at least using PyOpenGL, we need to
     # set the function pointer and invoke a function to actually register the callback, otherwise it
-    # would be very much like the C version of the code.    
+    # would be very much like the C version of the code.
     glutDisplayFunc(DrawGLScene)
-    
+
     # Uncomment this line to get full screen.
     # glutFullScreen()
 
     # When we are doing nothing, redraw the scene.
     glutIdleFunc(DrawGLScene)
-    
+
     # Register the function called when our window is resized.
     glutReshapeFunc(ReSizeGLScene)
-    
-    # Register the function called when the keyboard is pressed.  
+
+    # Register the function called when the keyboard is pressed.
     glutKeyboardFunc(keyPressed)
 
-    # Initialize our window. 
+    # Initialize our window.
     InitGL(640, 480)
 
-    # Start Event Processing Engine    
+    # Start Event Processing Engine
     glutMainLoop()
 
 # Print message to console, and kick off the main to get it rolling.
 if __name__ == "__main__":
     print "Hit ESC key to quit."
     main()
-        
+
